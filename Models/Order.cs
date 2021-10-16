e3fe2a77bcca4508c97cd82803f216714dfcc87e
RodicaMedan <rodica.medan@stud.ubbcluj.ro>
2021-10-16 12:17:50 +0300

    Lab 3

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,7 +9,7 @@ namespace Medan_Rodica_Lab2_EB.Models
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
-
+        public DateTime OrderDate { get; set; }
         public Customer Customer { get; set; }
         public Book Book { get; set; }
     }
